993a92903f0335d4824b50eed4076fe150aae72f
Dhruv <dmittal1902@gmail.com>
2024-09-28 18:06:28 +0530

    This is the Initial Implementation of the Filesystem.
    There are some things that are uncertain, for instance is the whole system
    insensitive or is only the searchLikeMatch supposed to be insensitive
    
    Moreover, both search functions can be optimized to use a trie like structure,
    more time needs to be spent on that

--- file_system_manager.js
+++ file_system_manager.js
@@ -12,6 +12,11 @@ class FileSystemManager {
      */
     constructor(rootName) {
         this.root = new Folder(rootName);
+        
+        // The Keys of this object will be the names of the files and folders
+        // The values will be the actual File or Folder objects
+        this.index = {};
+        this.index[rootName.toLowerCase()] = this.root;
     }
 
 
@@ -23,7 +28,33 @@ class FileSystemManager {
      * @param {boolean} isFolder true if adding a folder, false if adding a file
      */
     addFileOrFolder = (parentFolderName, name, isFolder) => {
+        let parentFolder = this.index[parentFolderName.toLowerCase()];
+        if (!parentFolder) {
+            // throw new Error(`Parent folder with name ${parentFolderName} does not exist`);
+            return null;
+        }
+        
+        // This isn't a limitation of usual filesystems, however our constrains 
+        // depend on parentFolderName, so we can't have two folders with the same name
+        // anywhere on the system
+        if(this.index[name.toLowerCase()]) {
+            return null;
+            // throw new Error(`Folder/File with name ${name} already exists`);
+        }
 
+        if (isFolder) {
+
+
+            let folder = new Folder(name);
+            parentFolder.addItem(folder);
+
+            // index is case insensitive, like our filesystem.
+            this.index[name.toLowerCase()] = folder;
+        } else {
+            let file = new File(name);
+            parentFolder.addItem(file);
+            this.index[name.toLowerCase()] = file;
+        }
     }
 
 
@@ -34,6 +65,20 @@ class FileSystemManager {
      * @param {string} destinationFolderName the name of the destination folder
      */
     moveFileOrFolder = (sourceName, destinationFolderName) => {
+        let source = this.index[sourceName.toLowerCase()];
+        if (!source) {
+            return;
+            // throw new Error(`File or folder with name ${sourceName} does not exist`);
+        }
+
+        let destinationFolder = this.index[destinationFolderName.toLowerCase()];
+        if (!destinationFolder) {
+            return null;
+            // throw new Error(`Destination folder with name ${destinationFolderName} does not exist`);
+        }
+
+        source.parent.removeItem(source);
+        destinationFolder.addItem(source);
 
     }
 
@@ -45,7 +90,13 @@ class FileSystemManager {
      * @returns {[]string} a list of names of files and folders within the specified folder
      */
     listContents = (folderName) => {
+        let folder = this.index[folderName];
+        if (!folder) {
+            return [];
+            // throw new Error(`Folder with name ${folderName} does not exist`);
+        }
 
+        return folder.items.map(item => item.name);
     }
 
 
@@ -55,7 +106,26 @@ class FileSystemManager {
      * @returns {[]string} a list representing the directory structure
      */
     listDirectoryStructure = () => {
+        // directory structure seems to be like a tree, where we have + for a folder
+        // - for a file and the number of spaces indicates the depth of the file/folder
 
+        let result = [];
+        let listRecursive = (item, depth) => {
+            let prefix = item.isFolder ? "+" : "-";
+            let spaces = " ".repeat(2*depth);
+            result.push(`${spaces}${prefix} ${item.name}`);
+            
+            if(!item.isFolder){
+                return null;
+            }
+
+            for (let files of item.items) {
+                listRecursive(files, depth + 1);
+            }
+        };
+
+        listRecursive(this.root, 0);
+        return result;
     }
 
 
@@ -67,6 +137,18 @@ class FileSystemManager {
      * @returns {string} the name of the file if found, null otherwise
      */
     searchFileExactMatch = (folderName, fileName) => {
+        let folder = this.index[folderName.toLowerCase()];
+        if (!folder) {
+            return null;
+            // throw new Error(`Folder with name ${folderName} does not exist`);
+        }
+
+        let item = folder.getItem(fileName);
+        if (!item) {
+            return null;
+        }
+
+        return item.name;
 
     }
 
@@ -79,7 +161,27 @@ class FileSystemManager {
      * @returns {[]string} a list of file names that match the pattern
      */
     searchFileLikeMatch = (folderName, pattern) => {
+        let folder = this.index[folderName.toLowerCase()];
+        if (!folder) {
+            return null;
+            // throw new Error(`Folder with name ${folderName} does not exist`);
+        }
 
+        let result = [];
+
+        let searchRecursive = (folder) => {
+            for (let item of folder.items) {
+                if (item.name.toLowerCase().includes(pattern.toLowerCase())) {
+                    result.push(item.name);
+                }
+                if (item.isFolder) {
+                    searchRecursive(item);
+                }
+            }
+        };
+
+        searchRecursive(folder);
+        return result;
     }
 }
 
